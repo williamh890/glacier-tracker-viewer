bcce813865660e352b4793617898f9bb5ea19d39
William Horn <wbhorn@alaska.edu>
2018-04-07 15:41:32 -0800

    View float encoded png in browser

---
+++ fragment.glsl
@@ -0,0 +1,29 @@
+precision mediump float;
+
+// our texture
+uniform sampler2D u_image;
+uniform float max_velocity;
+
+// the texCoords passed in from the vertex shader.
+varying vec2 v_texCoord;
+
+float delta = 0.00001;
+
+void main() {
+    vec4 lookup = texture2D(u_image, v_texCoord);
+
+    float vel = dot( lookup, vec4(1.0, 1./255.0, 1./65025.0, 1./16581375.0) );
+
+    vel = clamp(vel, 0., max_velocity) / max_velocity;
+
+    vec4 max_color = vec4(252., 253., 164., 255.) / 255.;
+    vec4 min_color = vec4(0., 53., 110., 255.) / 255.;
+
+    vec4 out_color = mix(min_color, max_color, vel);
+
+    if (vel < delta && vel > -delta) {
+        out_color.a = 0.;
+    }
+
+    gl_FragColor = out_color;
+}
---
+++ main.js
@@ -0,0 +1,191 @@
+
+let gl;
+
+function main() {
+    const canvas = document.querySelector("#canvas");
+
+    gl = canvas.getContext("webgl");
+
+    // Only continue if WebGL is available and working
+    if (!gl) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    //const fbo = setupFrameBuffer(gl);
+
+    //gl.bindFramebuffer(gl.FRAMEBUFFER, rttFramebuffer);
+    //drawSceneOnLaptopScreen();
+
+    //gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    let [vert, frag] = ["", ""];
+    let [vDone, fDone] = [false, false];
+
+    fetch('vertex.glsl')
+        .then(data => data.text())
+        .then(v => vert = v)
+        .then(() => {
+            fetch('fragment.glsl')
+                .then(data => data.text())
+                .then(f => frag = f)
+                .then(() => {
+                    let image = new Image();
+                    image.src = "raw-tiff.png"
+
+                    image.onload = () => make(image, vert, frag)
+                })
+        });
+}
+
+
+function make(image, vert, frag) {
+    const vertShader = createShader(gl, gl.VERTEX_SHADER, vert);
+    const fragShader = createShader(gl, gl.FRAGMENT_SHADER, frag);
+
+    const program = createProgram(gl, vertShader, fragShader);
+
+    var texCoordBuffer = makeImage(program, image);
+
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var maxVelocityLocation = gl.getUniformLocation(program, "max_velocity");
+    var deltaLocation = gl.getUniformLocation(program, "max_velocity");
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(program);
+
+    let maxVelSlider = document.getElementById("maxVelocity");
+    let deltaSlider = document.getElementById("delta");
+    gl.uniform1f(maxVelocityLocation, maxVelSlider.value);
+    gl.uniform1f(deltaLocation, delta.value);
+
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionAttributeLocation, size, type, normalize, stride, offset)
+
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+
+    maxVelSlider.addEventListener("mouseup", e => {
+        console.log(maxVelSlider.value);
+        gl.uniform1f(maxVelocityLocation, slider.value);
+
+        gl.drawArrays(primitiveType, offset, count);
+    });
+
+    deltaSlider.addEventListener("mouseup", e => {
+        console.log(deltaSlider.value);
+        gl.uniform1f(deltaLocation, slider.value);
+
+        gl.drawArrays(primitiveType, offset, count);
+    });
+
+}
+
+function makeImage(program, image) {
+    // look up where the texture coordinates need to go.
+    var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+
+    // provide texture coordinates for the rectangle.
+    var texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        0.0,  0.0,
+        1.0,  0.0,
+        0.0,  1.0,
+        0.0,  1.0,
+        1.0,  0.0,
+        1.0,  1.0]), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(texCoordLocation);
+    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+    // Create a texture.
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Set the parameters so we can render any size image.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    // Upload the image into the texture.
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+    return texCoordBuffer;
+}
+
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function setupFrameBuffer() {
+    const fbo = gl.createFramebuffer();
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    fbo.width = 512;
+    fbo.height = 512;
+
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, fbo.width, fbo.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+    var renderbuffer = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, renderbuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, fbo.width, fbo.height);
+
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, rttTexture, 0);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, renderbuffer);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    return fbo;
+}
+
